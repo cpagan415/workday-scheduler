1f3436a9133b567e1d488f20f3e3b2a7d85e36c4
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-26 21:57:09 -0400

    swtich js time to moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,10 @@
 
-function currentTime() {
-//looking to find a way to display the current day and time 
-//https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-1.php
-//used this following resource instead of moment.js 
-var currDay = new Date();
-var month = {month : 'long'};
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
-//reference above for month
-var day = currDay.getDay(week);
-var numDate = currDay.getUTCDate();
-var numYear =currDay.getFullYear();
-var hour = currDay.getHours();
-var minute = currDay.getMinutes();
-var week= ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-if(minute < 10) {
-    var lead = 0+ '';
-    minute = lead + minute;
-}
-else
-{
-    mintue = minute;
-}
-if(hour <= 12){
-    var print = new Intl.DateTimeFormat('en-Us', month).format()  + ' ' + numDate + ' ' + numYear + ' '+ week[day] + ' ' + hour + ':' + minute + "AM";
-}
-else
-{
-    var print= new Intl.DateTimeFormat('en-Us', month).format() + ' ' + numDate + ' ' + numYear + ' ' + week[day] + ' ' + hour + ':' + minute + "AM"; 
-}
-
-
+//used moment.js to display current time
+//decided to switch from js to moment
+var currentTime = function(){
+var currTime = moment().format("MMMM Do, YYYY, hh:mm:ss a");
 var dateEl = document.getElementById('currentDay');
-dateEl.innerHTML = print;
-
+dateEl.innerHTML = currTime;
 }
 
 currentTime();
-
-
-//Step 2 displayed time blocks
