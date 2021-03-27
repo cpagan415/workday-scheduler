ded25c8abd536f2c1104c8537c21b8c303ae3c08
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-26 22:53:07 -0400

    adding to do task on a page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
 
 //used moment.js to display current time
 //decided to switch from js to moment
-var currentTime = function(){
-var currTime = moment().format("MMMM Do, YYYY, hh:mm:ss a");
+
+var currTime = moment().format("MMMM Do YYYY hh:mm a");
 var dateEl = document.getElementById('currentDay');
 dateEl.innerHTML = currTime;
-}
 
-currentTime();
+
+
+//adding to-do task on a page 
+var text = document.querySelector(".text");
+var textEl = document.createElement('textarea');
+text.appendChild(textEl);
+textEl.setAttribute('class', 'textBox');
+textEl.style.width = "100%";
+textEl.style.height = "100%";
+
+var saveText = document.querySelector(".saveText");
+var textBox = document.querySelector(".textBox");
+
+saveText.addEventListener("click", function(){
+    var pEl = document.createElement('p');
+    pEl.innerHTML = textBox.value;
+    textEl.replaceWith(pEl);
+    console.log(pEl);
+});
+
+
