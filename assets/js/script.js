be65e1d6ad94e9fad62e03643b0a70805a473379
Christina Pagan <enterprise@christinas-mbp.home>
2021-03-28 08:53:42 -0400

    placing user input in js object

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,40 @@ var currTime = moment().format("MMMM Do YYYY hh:mm a");
 var dateEl = document.getElementById('currentDay');
 dateEl.innerHTML = currTime;
 
+//saving to-do task on a page 
+//https://www.youtube.com/watch?v=rVyTjFofok0 used as reference
+const userInput = document.querySelector('.userInput');
+const btn = document.querySelector('.button');
+const storageDisplay = localStorage.getItem('userToDo');
+
+//https://www.youtube.com/watch?v=NxVCq4p0Kb0&t=400s
+//using this as reference to make an object for the tasks added to the form
+//I want to be able to assign each task a date in a object
+
+//an empty array to push the object into
+let taskToDo = [];
+
+var addTask = function(event) {
+    event.preventDefault();
+
+    let task = {
+        date: moment(),
+        task: document.querySelector('.userInput').value
+
+    };
+
+    //this will push the newly created task in to the task object
+    taskToDo.push(task);
+    console.log(taskToDo);
 
 
-//adding to-do task on a page 
-var text = document.querySelector(".text");
-var textEl = document.createElement('textarea');
-text.appendChild(textEl);
-textEl.setAttribute('class', 'textBox');
-textEl.style.width = "100%";
-textEl.style.height = "100%";
+    //localStorage
+    localStorage.setItem('userInput', JSON.stringify(taskToDo));
+}
 
-var saveText = document.querySelector(".saveText");
-var textBox = document.querySelector(".textBox");
+//adding an event listener for when user enters text 
+document.querySelector('.button').addEventListener('click', addTask);
 
-saveText.addEventListener("click", function(){
-    var pEl = document.createElement('p');
-    pEl.innerHTML = textBox.value;
-    textEl.replaceWith(pEl);
-    console.log(pEl);
-});
+
 
 
